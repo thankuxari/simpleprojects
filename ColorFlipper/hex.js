71e7719e45755470da6446218133a74fe999ff73
thankuxari <konstantinoseyosmos@gmail.com>
2024-03-13 10:08:42 +0200

    Color Flipper Commented

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
+
+    //All possible hex combinations
     const simpleColors = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
     const clickmeBtn = document.getElementById('clickmeBtn');
@@ -8,18 +10,20 @@ document.addEventListener('DOMContentLoaded',()=>{
     clickmeBtn.addEventListener('click',pickColor);
 
     function pickColor(){
+
+        //Clear previous title container
         colorTitleContainer.innerHTML = '';
         let colorString = '#'
-        //Ftiaxnei seira h1
+
+
         const colorTitle = document.createElement('h1');
+
         for(let i  = 0 ; i <= 5 ; i++){
             
-            //Dialegei Indexer
+            //Pick the random number
             const color = Math.floor(Math.random() * simpleColors.length);
             
-            
             colorString += simpleColors[color];
-            
         }
 
         colorTitle.innerHTML = 'Background color: ' + colorString;
--- ColorFlipper/index.js
+++ ColorFlipper/index.js
@@ -1,19 +1,26 @@
+//Wait for the DOM do be loaded
 document.addEventListener('DOMContentLoaded',()=>{
+
+    //An array of different colors to be picked for the background
     const simpleColors = ['red','cyan','#779b86','#d448f1','#d7b5de','green'];
 
     const clickmeBtn = document.getElementById('clickmeBtn');
     const main = document.getElementsByTagName('main')[0];
     const colorTitleContainer = document.createElement('div');
+
     colorTitleContainer.classList.add('colortitle-container');
+
     clickmeBtn.addEventListener('click',pickColor);
 
-    function pickColor(){
 
+    function pickColor(){
+        //Clear previous title container
         colorTitleContainer.innerHTML = '';
 
         const colorTitle = document.createElement('h1');
         const color = Math.floor(Math.random() * simpleColors.length);
 
+        //Change the background color
         document.body.style.backgroundColor = simpleColors[color];
         colorTitle.innerHTML = 'Background color: ' + simpleColors[color];
 
