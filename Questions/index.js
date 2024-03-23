a0bf565a8c4abde6ee683525131ca9a774dcf5db
thankuxari <konstantinoseyosmos@gmail.com>
2024-03-23 10:06:26 +0200

    Questions Finished

--- Questions/index.js
+++ Questions/index.js
@@ -1,1 +1,23 @@
-console.log('hello')
+const openBtns = document.querySelectorAll('.open-btn');
+const paragraph = document.querySelectorAll('p');
+const icon = document.querySelectorAll('.fa-solid.fa-plus');
+const hr = document.querySelectorAll('hr');
+
+console.log(paragraph);
+
+openBtns.forEach(function(btn,index){
+
+    btn.addEventListener('click',()=>{
+        if(icon[index].classList.contains('fa-plus')){
+            icon[index].classList.remove('fa-plus');
+            icon[index].classList.add('fa-minus');
+            paragraph[index].classList.add('active');
+            hr[index].classList.add('active');
+        }else{
+            icon[index].classList.add('fa-plus');
+            icon[index].classList.remove('fa-minus');
+            paragraph[index].classList.remove('active');
+            hr[index].classList.remove('active');
+        }
+    })
+});
